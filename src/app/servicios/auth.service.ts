5d9fce438e76f9ace027cf995e4633cb7b62545e
Mariana Naselli <nasellimariana@gmail.com>
2023-03-10 03:58:11 +0100

    Agregamos cookie para mantener la sesion del usuario

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   private currentUserSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private tokenKey = 'auth_token';
 
-  constructor() { }
+  constructor(private cookieService: CookieService) {
+    const token = this.cookieService.get(this.tokenKey);
+    if (token) {
+      // Si hay un token guardado en la cookie, actualizamos el estado del usuario logueado
+      this.currentUserSubject.next(true);
+    }
+  }
 
   login(username: string, password: string): Observable<boolean> {
     // Aquí validamos el usuario y la contraseña
     if (username === 'admin' && password === 'admin') {
       // Si son correctos, actualizamos el estado del usuario logueado
       this.currentUserSubject.next(true);
+      const token = 'GENERAR_TOKEN_JWT_AQUI';
+      this.cookieService.set(this.tokenKey, token);
       return this.currentUserSubject.asObservable();
     } else {
       // Si son incorrectos, retornamos un observable con valor false
@@ -25,6 +34,7 @@ export class AuthService {
   logout(): void {
     // Actualizamos el estado del usuario logueado
     this.currentUserSubject.next(false);
+    this.cookieService.delete(this.tokenKey);
   }
 
   isLoggedIn(): Observable<boolean> {

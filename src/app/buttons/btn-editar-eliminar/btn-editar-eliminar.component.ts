1e22b2d0fcbeeeb9513096c4ad717bc3f9bb73e4
Mariana Naselli <nasellimariana@gmail.com>
2023-03-10 21:38:06 +0100

    Agregamos sweetalert2

--- src/app/buttons/btn-editar-eliminar/btn-editar-eliminar.component.ts
+++ src/app/buttons/btn-editar-eliminar/btn-editar-eliminar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-btn-editar-eliminar',
@@ -10,6 +11,35 @@ export class BtnEditarEliminarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  confirmarEliminar(){
+    Swal
+      .fire({
+        title: '¡Atención!',
+        text: '¿Estás seguro de que quieres eliminar esta tarea?',
+        icon: 'warning',
+        confirmButtonText: 'Sí, eliminar!',
+        confirmButtonColor: 'red',
+        showCancelButton: true,
+        cancelButtonText: 'No, cancelar',
+        cancelButtonColor: 'grey',
+      })
+      .then((swal_result) => {
+        if (swal_result.isConfirmed) {
+          //LLAMAMOS AL SERVICIO PARA ELIMINAR LA TAREA
+          //this.OnDeleteTask.emit(task);
+          console.log("Eliminar")
+
+          //MOSTRAMOS UN MENSAJE AL USUARIO INFORMANDO QUE SE ELIMINÓ CORRECTAMENTE
+          Swal.fire({
+            title: '¡Información!',
+            text: 'Se eliminó la tarea correctamente',
+            icon: 'success',
+            confirmButtonText: 'Ok',
+            confirmButtonColor: 'green',
+          });
+        }
+      });
+  }
 }

af0bac1a426eb009183d0da9ab81aecb1e158933
Mariana Naselli <nasellimariana@gmail.com>
2023-03-13 14:37:34 +0100

    Oculto boton nuevo cuando el usuario no esta logueado

--- src/app/componentes/secciones/secciones.component.ts
+++ src/app/componentes/secciones/secciones.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-secciones',
@@ -9,14 +10,17 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class SeccionesComponent implements OnInit {
 
   @ViewChild('modalItem') modalItem: any;
-
+  isLoggedIn = false;
 
   constructor(
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private authService: AuthService,
   ) {}
 
   ngOnInit(): void {
-
+    this.authService.isLoggedIn().subscribe((isLoggedIn: boolean) => {
+      this.isLoggedIn = isLoggedIn;
+    });
   }
   nuevoItem(content: any): void {
     this.modalItem = this.modalService.open(content, {

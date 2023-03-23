0fe51f0bca84d2322d521735dbb13ea6491a8ea0
Mariana Naselli <nasellimariana@gmail.com>
2023-03-23 01:23:04 +0100

    Agrego paquetes y arhivos de config. firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-web-d6b0.onrender.com/'
+  apiUrl: 'https://portfolio-web-d6b0.onrender.com', // OJO!! SIN BARRA AL FINAL
+  firebaseConfig: {
+    apiKey: "AIzaSyDxnEu_HpEKw2xcm2R3aOOvKdy62b4y0co",
+    authDomain: "portfolio-web-2559b.firebaseapp.com",
+    projectId: "portfolio-web-2559b",
+    storageBucket: "portfolio-web-2559b.appspot.com",
+    messagingSenderId: "343194159785",
+    appId: "1:343194159785:web:d124f9efe6d0c85ea877b8",
+    measurementId: "G-QZ4VTQ7FW5"
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,7 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// Initialize Firebase
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
